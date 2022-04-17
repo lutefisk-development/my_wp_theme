9b91928c7667ea76e5456290448a0e8278a4cbb9
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2022-04-17 11:10:43 +0200

    Update webpack.mix.js
    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,15 @@
-// webpack.mix.js
+/*
+ * Laravel Mix
+ *
+ * https://laravel-mix.com/
+ */
 
+const webpack = require( 'webpack' );
 let mix = require('laravel-mix');
-require( 'laravel-mix-modernizr' );
+//require( 'laravel-mix-modernizr' );
 require( 'laravel-mix-copy-watched' );
 
-mix.js('assets/src/scripts/app.js', 'assets/dist/js').modernizr()
+mix.js('assets/src/scripts/app.js', 'assets/dist/js')
    .sass('assets/src/sass/style.scss', 'assets/dist/css')
    .setPublicPath('./assets/dist')
    .sourceMaps()
@@ -14,3 +19,15 @@ mix.js('assets/src/scripts/app.js', 'assets/dist/js').modernizr()
    .options({
      processCssUrls: false
    });
+
+mix.setResourceRoot( '../' );
+
+mix.webpackConfig({
+	plugins: [
+		new webpack.ProgressPlugin( ( percentage, message ) => {
+			if ( 0 === percentage * 100 % 5 ) {
+				console.log( `${( percentage * 100 ).toFixed()}% ${message}` );
+			}
+		})
+	]
+});
