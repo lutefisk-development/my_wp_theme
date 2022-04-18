fcfa80ef1be457da2cc46a15b9ac26a4c1780e27
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2022-04-18 13:36:26 +0200

    Create Init.php
    Added Init class

---
+++ inc/Init.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Lutefisk;
+
+use Lutefisk\Setup\Enqueue;
+
+final class Init {
+
+	/**
+	 * Store all classes in an array
+	 *
+	 * @return void
+	 */
+	public static function get_classes() {
+		return [
+			Enqueue::class,
+		];
+	}
+
+	/**
+	 * Loop through the classes, instantiate them, and call the register() method if it exists
+	 *
+	 * @return void
+	 */
+	public static function register_classes() {
+		foreach ( self::get_classes() as $class ) {
+			$class = self::instantiate( $class );
+
+			if ( method_exists( $class, 'register' ) ) {
+				$class->register();
+			}
+		}
+	}
+
+	/**
+	 * Instantiate the class
+	 *
+	 * @param class $class
+	 * @return class instance
+	 */
+	private static function instantiate( $class ) {
+		return new $class;
+	}
+}
