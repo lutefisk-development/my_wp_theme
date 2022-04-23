7b75a5401540349254cc981c82d597ba07f3d043
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2022-04-23 15:59:38 +0200

    Update index.php
    Updated index.php file

--- index.php
+++ index.php
@@ -1,2 +1,40 @@
 <?php
-// Silence is golden
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package lute
+ */
+
+get_header();
+?>
+
+<main id="site-content">
+	<?php if ( have_posts() ) :
+
+		if ( is_home() && ! is_front_page() ) : ?>
+			<h1 class="page-title"><?php single_post_title(); ?></h1>
+		<?php endif;
+
+		/* Start the loop */
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+		endwhile;
+
+		the_posts_navigation();
+
+	else :
+		get_template_part( 'template-parts/content', 'none' );
+	endif; ?>
+</main><!-- /#site-content -->
+
+<?php
+get_footer();
