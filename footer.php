ea5dee660304443d11041a140e6730776c4c5e6d
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2022-04-23 16:00:20 +0200

    Update footer.php
    Updated footer.php file

--- footer.php
+++ footer.php
@@ -1,2 +1,20 @@
 <?php
-// Silence is golden
+/**
+ * The footer for our theme
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package lute
+ */
+
+?>
+
+<footer id="site-footer">
+	<!-- HERE WILL BE FOOTER -->
+</footer><!-- /#site-footer -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
