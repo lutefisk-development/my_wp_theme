1857ba52ab6b856d3c214f6f5709c259eed1bae1
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2022-04-23 16:00:04 +0200

    Update header.php
    Updated header.php file

--- header.php
+++ header.php
@@ -1,2 +1,26 @@
 <?php
-// Silence is golden
+/**
+ * The header for our theme
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package lute
+ */
+
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+	<header id="site-header">
+		<!-- HERE WILL BE HEADER -->
+	</header><!-- /#site-header -->
