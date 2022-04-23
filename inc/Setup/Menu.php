736091bbc6f329906c6958e23f5211e1f37cde3b
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2022-04-23 16:01:39 +0200

    Update Menu.php
    Added Menu class

--- inc/Setup/Menu.php
+++ inc/Setup/Menu.php
@@ -1,2 +1,42 @@
 <?php
-// Silence is golden
+
+namespace Lutefisk\Setup;
+
+/**
+ * Register menus and sidebars
+ *
+ */
+class Menu {
+	public function register() {
+		add_action( 'after_theme_setup', [ $this, 'lf_register_menus' ] );
+		add_action( 'widgets_init', [ $this, 'lf_widgets_init' ] );
+	}
+
+	/**
+	 * Register nav menues
+	 *
+	 */
+	public function lf_register_menus() {
+		register_nav_menus([
+			'header' => esc_html__( 'Header', 'lute' ),
+			'footer' => esc_html__( 'Footer', 'lute' ),
+		]);
+	}
+
+	/**
+	 * Register widget area.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+	 */
+	public function lf_widgets_init() {
+		register_sidebar([
+			'name'          => esc_html__( 'Sidebar', 'lute' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here', 'lute' ),
+			'before_widget' => '<section id="%1$s" class="widgets %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		]);
+	}
+}
